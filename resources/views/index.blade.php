f2e8ea25020384de8244be01846bd7e836a96c07
keisukefukuchi <kekke53530708@gmail.com>
2022-04-22 14:09:33 +0900

    Fix: Shop-view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,22 @@
 @section('content')
     <div class="container">
         <div class="wrapper">
-            @foreach($items as $item)
+            @foreach ($items as $item)
                 <div class="card">
-                    <img class="shop-img" src="{{$item['shop']->img_url}}" alt="">
-                    <h1>{{$item['shop']->name}}</h1>
-                    <p>#{{$item['area_name']}}</p>
-                    <p>#{{$item['genre_name']}}</p>
+                    <img class="shop-img" src="{{ $item['shop']->img_url }}" alt="">
+                    <div class="content">
+                        <h1 class="shop-name">{{ $item['shop']->name }}</h1>
+                        <div class="hashtag">
+                            <p class="area-name">#{{ $item['area_name'] }}</p>
+                            <p class="genre-name">#{{ $item['genre_name'] }}</p>
+                        </div>
+                        <div class="shop-link">
+                            <button class="shop-details">詳しく見る</button>
+                            <a href="" class="shop-like">
+                                <i class="fa-solid fa-heart fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             @endforeach
         </div>
