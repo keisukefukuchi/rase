c7d6d677a3f4b370ad282524a39d21f7a62d0d3b
keisukefukuchi <kekke53530708@gmail.com>
2022-04-25 02:54:46 +0900

    Add: Review-view

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -23,23 +23,43 @@
                 @csrf
                 <div class="reservation-input">
                     <h1 class="reservation-head">予約</h1>
-                    <input type="hidden" value="{{$shop->id}}" name="shop_id">
+                    <input type="hidden" value="{{ $shop->id }}" name="shop_id">
                     <div class="reservation-day">
-                        <input type="date" name="start_date" value="{{$tomorrow}}" id="tomorrow" min="{{$tomorrow}}">
+                        <input type="date" name="start_date" value="{{ $tomorrow }}" id="tomorrow"
+                            min="{{ $tomorrow }}">
                     </div>
                     <div class="reservation-time">
-                        <input type="time" name="start_time" value="{{$time}}">
+                        <input type="time" name="start_time" value="{{ $time }}">
                     </div>
                     <div class="reservation-person">
                         <select name="num_of_users">
-                            @for($i=1;$i<=20;$i++)
-                                <option value="{{$i}}">{{$i}}人</option>
+                            @for ($i = 1; $i <= 20; $i++)
+                                <option value="{{ $i }}">{{ $i }}人</option>
                             @endfor
                         </select>
                     </div>
                 </div>
                 <button class="detail-button">予約する</button>
             </form>
+        </div>
+        <div class="shop-review">
+            <h1>口コメ</h1>
+            <div class="shop-review-card">
+                <div class="shop-review-flex">
+                    <div class="shop-reivew-left">
+                        <i class="fa-solid fa-star fa-2x"></i>
+                        <i class="fa-solid fa-star fa-2x"></i>
+                        <i class="fa-solid fa-star fa-2x"></i>
+                        <i class="fa-solid fa-star fa-2x"></i>
+                        <i class="fa-solid fa-star fa-2x"></i>
+                    </div>
+                    <div class="shop-review-right">
+                        <p class="shop-review-comment">サンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+                            サンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
