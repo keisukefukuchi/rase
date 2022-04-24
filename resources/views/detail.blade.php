a939cb3fff2446f4ddff57dcf3d608adb6239e00
keisukefukuchi <kekke53530708@gmail.com>
2022-04-24 11:48:33 +0900

    Fix: detail-view

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -8,10 +8,10 @@
             <div class="detail-left">
                 <div class="detail-head">
                     <a href="/" class="back-shop-page"><</a>
-                    <h1 class="detail-shop-name">{{$shop->name}}</h1>
+                    <h1 class="detail-shop-name">{{ $shop->name }}</h1>
                 </div>
                 <div class="detail-shop_image">
-                    <img src="{{$shop->img_url}}" alt="">
+                    <img src="{{ $shop->img_url }}" alt="">
                 </div>
                 <div class="detail-hashtag">
                     <p class="area-name">#{{ $shop->area->name }}</p>
@@ -20,7 +20,8 @@
                 <p class="detail-shop-content">{{ $shop->description }}</p>
             </div>
             <form class="detail-right">
-                <h1 class="reservation-head">予約</h1>
+                <div class="reservation-input">
+                    <h1 class="reservation-head">予約</h1>
                     <div class="reservation-day">
                         <input type="date">
                     </div>
@@ -28,9 +29,14 @@
                         <input type="time">
                     </div>
                     <div class="reservation-person">
-
+                        <select name="example">
+                            @for($i=1;$i<=20;$i++)
+                                <option value="">{{$i}}人</option>
+                            @endfor
+                        </select>
                     </div>
-                    <button class="detail-button">予約する</button>
+                </div>
+                <button class="detail-button">予約する</button>
             </form>
         </div>
     </div>
