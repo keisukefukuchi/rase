1cec671bf367bb2242e4dfdcb089dacd2fa5f100
keisukefukuchi <kekke53530708@gmail.com>
2022-04-24 20:02:01 +0900

    Fix: thanks-view

---
+++ resources/views/layouts/thanks.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>
+        @yield('title')
+    </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+</head>
+
+<body>
+    <header>
+        <div class="header-guest">
+            <nav class="nav" id="nav">
+                <ul>
+                    <li><a class="drawer_link" href="/">HOME</a></li>
+                    <li><a class="drawer_link" href="/register">Registration</a></li>
+                    <li><a class="drawer_link" href="/login">Login</a></li>
+                </ul>
+            </nav>
+            <div class="menu" id="menu">
+                <span class="menu__line--top"></span>
+                <span class="menu__line--middle"></span>
+                <span class="menu__line--bottom"></span>
+            </div>
+            <h1 class="header-title">Rese</h1>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.thanks')
 
 @section('title', '会員登録画面')
 
