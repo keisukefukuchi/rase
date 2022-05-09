4dbd4dfb449115c5b4387f0ef902a562e029df2f
keisukefukuchi <kekke53530708@gmail.com>
2022-05-10 04:51:12 +0900

    Fix: Password Reset Email Authentication

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,33 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.app')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@section('title', '会員登録画面')
+
+@section('content')
+    <div class="register-container">
+        <div class="register-wrapper">
+            <div class="register-box">
+                <p class="register-title">Registration</p>
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4 reset-path" :status="session('status')" />
+                <form class="register-form" method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div>
+                        <div class="email">
+                            @error('email')
+                                <p class="error">{{ $message }}</p>
+                            @enderror
+                            <i class="icon fa-solid fa-envelope fa-lg"></i>
+                            <input class="register-input register-email" type="email" name="email" placeholder="Email" value="">
+                        </div>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <x-button class="password-reset-bottom">
+                            {{ __('Email Password Reset Link') }}
+                        </x-button>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+@endsection
