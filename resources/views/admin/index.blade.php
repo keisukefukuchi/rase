b260ada9a4e00a92c8bc91e626937ef8159222f3
keisukefukuchi <kekke53530708@gmail.com>
2022-08-25 14:57:44 +0900

    add: multi

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,2 +1,30 @@
-@extends('layouts.thanks')
+@extends('layouts.admin')
+
+@section('content')
 管理者ページ
+<div class="dashboard box-shadow">
+    <h2 class="dashboard-ttl">店舗代表者登録</h2>
+    <form class="register-form" action="" method="POST">
+      @csrf
+      <div class="register-input-wrap">
+        <img src="{{ asset('image/representative.png') }}" alt="name" class="register-img">
+        <input type="text" name="name" class="register-input" value="{{ old('name') }}" placeholder="Name">
+      </div>
+      @error('name')<label class="error">{{ $message }}</label>@enderror
+      <div class="register-input-wrap">
+        <img src="{{ asset('image/email.png') }}" alt="email" class="register-img">
+        <input type="email" name="email" class="register-input" value="{{ old('email') }}" placeholder="Email">
+      </div>
+      @error('email')<label class="error">{{ $message }}</label>@enderror
+      <div class="register-input-wrap">
+        <img src="{{ asset('image/password.png') }}" alt="password" class="register-img">
+        <input type="password" name="password" class="register-input" placeholder="Password">
+      </div>
+      @error('password')<label class="error">{{ $message }}</label>@enderror
+      <div class="register-btn-wrap">
+        <button type="submit" class="register-btn">登録</button>
+      </div>
+    </form>
+  </div>
+@endsection
+
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>
+        @yield('title')
+    </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+</head>
+
+<body>
+    <header>
+        <div class="header-guest">
+            <nav class="nav" id="nav">
+                <ul>
+                    <li><a class="drawer_link" href="/">HOME</a></li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button class="drawer_link drawer_button">Logout</button>
+                        </form>
+                    </li>
+                </ul>
+            </nav>
+            <div class="menu" id="menu">
+                <span class="menu__line--top"></span>
+                <span class="menu__line--middle"></span>
+                <span class="menu__line--bottom"></span>
+            </div>
+            <h1 class="header-title">Rese</h1>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
